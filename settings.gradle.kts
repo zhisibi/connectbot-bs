7d3bef657bb1aa05f200b93ad9b374534c14e8df
zhisibi <zhisibi@163.com>
2026-03-31 21:17:37 +0800

    build: add Aliyun/Huawei maven mirrors for AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,23 @@
 pluginManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://mirrors.huaweicloud.com/repository/maven")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
         google()
         gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://mirrors.huaweicloud.com/repository/maven")
+        google()
+        mavenCentral()
     }
 }
 
