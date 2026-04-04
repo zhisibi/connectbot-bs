54728f8f346dcb4d852e5858c02b3e0493295faa
zhisibi <zhisibi@163.com>
2026-04-04 10:36:13 +0800

    fix: downgrade Kotlin Gradle Plugin to 1.9.22 for AGP 8.6.0 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.57")
     }
 }
